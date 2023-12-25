1a31ed6213ba8655de602bcc1937d2a899276a31
praveenece431 <97307468+praveenece431@users.noreply.github.com>
2023-12-25 22:54:40 +0530

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,4 +1,4 @@
-package com.abhishek;
+package com.praveen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "Ya Hoo..! I have successfuly built and deployed the sprint boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes Jenkins");
         return "index";
     }
 
